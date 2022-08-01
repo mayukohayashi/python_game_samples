4dbf8c33a23d7f22c9d5c2f41b7497e51ac02442
mayuko.hayashi <mayuko.hayashi@tongullman.co.jp>
2022-08-01 15:51:44 +0900

    add: die and spown

--- kivy_snake/kivy_snake.py
+++ kivy_snake/kivy_snake.py
@@ -5,17 +5,19 @@ from kivy.clock import Clock
 from kivy import properties as kp
 from kivy.uix.widget import Widget
 from collections import defaultdict
-# from kivy.lang import Builder  # .kv使うため
+from kivy.animation import Animation
+from random import randint
+# from kivy.lang import Builder  # わざわざいらんかった
 
 ## Windowsサイズ
 SPRITE_SIZE = sp(20)
 WIDTH = int(Window.width / SPRITE_SIZE)
 HEIGHT = int(Window.height / SPRITE_SIZE)
 
-## SNAKE自体の長さやスピード
+## SNAKE自体の長さやスピード（挙動）
 LENGTH = 4
 MOVESPEED = .1
-
+ALPHA = .5
 ### SNAKEの方向指示
 UP = 'up'
 DOWN = 'down'
@@ -43,9 +45,13 @@ class Snake(App):
   snake =  kp.ListProperty()
   length = kp.NumericProperty(LENGTH)
 
+  ### snakeがたべる餌
   food = kp.ListProperty([0, 0])
 
   direction = kp.StringProperty(RIGHT, options = (UP, DOWN, RIGHT, LEFT))
+
+  ###　死んだ時の挙動用（snake.kv）
+  alpha = kp.NumericProperty(0)
 
   def on_start(self):
     Clock.schedule_interval(self.move, MOVESPEED)
@@ -60,6 +66,10 @@ class Snake(App):
       if not sprite.parent:
         self.root.add_widget(sprite)
 
+  @property
+  def new_head_location(self):
+    return [randint(2, dim - 2) for dim in [WIDTH, HEIGHT]]
+
   def move(self, *args):
     new_head = [sum(x) for x in zip(self.head, direction_values[self.direction])]
     if not self.check_in_bounds(new_head):
@@ -70,6 +80,13 @@ class Snake(App):
   ## 端にあたったら死ぬ
   def check_in_bounds(self, pos):
     return all(0 <= pos[x] < dim for x, dim in enumerate([WIDTH, HEIGHT]))
+
+  def die(self):
+    self.alpha = ALPHA
+    Animation(alpha = 0, duration = MOVESPEED).start(self)
+    self.snake.clear()
+    self.root.clear_widgets()
+    self.head = self.new_head_location
 
 
 if __name__ == '__main__':
